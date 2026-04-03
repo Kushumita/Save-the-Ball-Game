a3614eed687ee0aa6141e7be800a3f3b39758fdb
Kushumita <kushumitamahato5464@gmail.com>
2026-04-03 13:53:39 +0530

    Updated Game

--- Game2/Ball.cpp
+++ Game2/Ball.cpp
@@ -3,42 +3,23 @@
 Ball::Ball(float startX, float startY, float radius) {
     m_Position.x = startX;
     m_Position.y = startY;
+    m_Shape.setRadius(radius);
     m_Shape.setPosition(m_Position);
     m_InitialPosition = m_Position;
-    m_Shape.setRadius(radius);
 }
+Vector2f     Ball::getPosition() { return m_Position; }
+FloatRect    Ball::getBounds() {return m_Shape.getGlobalBounds();}
 CircleShape &Ball::getShape() { return m_Shape; }
-Vector2f    &Ball::getPosition() { return m_Position; }
-FloatRect    Ball::getBounds() { return m_Shape.getGlobalBounds(); }
-
-void Ball::reboundTop() {
-    m_Position.y = 1;
-    m_DirectionY = -m_DirectionY;
+void         Ball::bounceSides() { m_DirectionX = -m_DirectionX; }
+void         Ball::bounceByBat() { m_DirectionY = -m_DirectionY; }
+void         Ball::bounceTop() { m_DirectionY = -m_DirectionY; }
+void         Ball::bounceBottom() { m_Position = m_InitialPosition; }
+void         Ball::update(Time dt) {
+    m_Position.x+= m_DirectionX * m_Speed*dt.asSeconds();
+    m_Position.y+= m_DirectionY * m_Speed*dt.asSeconds();
     m_Shape.setPosition(m_Position);
 }
-void Ball::reboundBottom() { m_DirectionY = -m_DirectionY; }
-void Ball::reboundSides(Vector2i resolution) { 
-    if(m_DirectionX<0){
-        m_Position.x = 1;
-    }else{
-        m_Position.x = resolution.x-m_Shape.getLocalBounds().width -1;
-    }
-    m_Shape.setPosition(m_Position);
-    m_DirectionX = -m_DirectionX; 
-}
-void Ball::reboundbyBat(int batPosY) {
-    m_Position.y = batPosY - 1 - m_Shape.getLocalBounds().height;
-    m_Shape.setPosition(m_Position);
-    m_DirectionY = -m_DirectionY;
-}
-
-void Ball::update(Time dt) {
-    m_Position.x = m_Shape.getPosition().x + m_DirectionX * dt.asSeconds() * m_Speed;
-    m_Position.y = m_Shape.getPosition().y + m_DirectionY * dt.asSeconds() * m_Speed;
+void Ball::reset(){
+    m_Position=m_InitialPosition;
     m_Shape.setPosition(m_Position);
 }
-
-        void Ball::reset(){
-            m_Position = m_InitialPosition;
-            m_Shape.setPosition(m_Position);
-        }
