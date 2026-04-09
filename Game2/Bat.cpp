5725bb03d53021961dd1056eae93fa6f64412234
Kushumita <kushumitamahato5464@gmail.com>
2026-04-09 19:47:54 +0530

    Updated Game

--- Game2/Bat.cpp
+++ Game2/Bat.cpp
@@ -1,47 +1,48 @@
+#pragma once
 #include <SFML/Graphics.hpp>
 using namespace sf;
 
 class Bat {
   private:
-    RectangleShape m_Shape;
     Vector2f       m_Position;
-    Vector2f       m_Initial_Position;
-    float          m_Speed = 500.0f;
-
-    bool m_MovingLeft  = false;
-    bool m_MovingRight = false;
+    Vector2f       m_InitialPosition;
+    RectangleShape m_Shape;
+    float          m_Speed       = 1000;
+    bool           m_MovingLeft  = false;
+    bool           m_MovingRight = false;
 
   public:
-    Bat(float startX, float startY, int width, int height);
-    RectangleShape getShape();
-    FloatRect      getBounds();
-    void           moveLeft();
-    void           stopLeft();
-    void           moveRight();
-    void           stopRight();
-    void           update(Time dt,Vector2i resolution);
+    Bat(float startX, float startY, float width, float height);
+    RectangleShape &getShape();
+    Vector2f       &getPosition();
+    FloatRect       getBounds();
+    void            movingLeft();
+    void            movingRight();
+    void            stopLeft();
+    void            stopRight();
+
+    void update(Time dt,Vector2i resolution);
 };
 
-Bat::Bat(float startX, float startY, int width, int height) {
-    m_Position.x       = startX;
-    m_Position.y       = startY;
-    m_Initial_Position = m_Position;
+Bat::Bat(float startX, float startY, float width, float height) {
+    m_Position.x = startX;
+    m_Position.y = startY;
     m_Shape.setPosition(m_Position);
+    m_InitialPosition = m_Position;
     m_Shape.setSize(Vector2f(width, height));
 }
-RectangleShape Bat::getShape() { return m_Shape; }
+RectangleShape &Bat::getShape() { return m_Shape; }
+Vector2f       &Bat::getPosition() { return m_Position; }
+FloatRect       Bat::getBounds() { return m_Shape.getGlobalBounds(); }
+void            Bat::movingLeft() { m_MovingLeft = true; }
+void            Bat::movingRight() { m_MovingRight = true; }
+void            Bat::stopLeft() { m_MovingLeft = false; }
+void            Bat::stopRight() { m_MovingRight = false; }
 
-FloatRect Bat::getBounds() {return m_Shape.getGlobalBounds();}
-void Bat::moveLeft() { m_MovingLeft = true; }
-void Bat::stopLeft() { m_MovingLeft = false; }
-void Bat::moveRight() { m_MovingRight = true; }
-void Bat::stopRight() { m_MovingRight = false; }
 void Bat::update(Time dt,Vector2i resolution) {
-    if(m_MovingLeft && m_Position.x>0){
-        m_Position.x-= m_Speed*dt.asSeconds();
-    }
-    if(m_MovingRight && (m_Position.x+m_Shape.getGlobalBounds().width)<resolution.x){
-        m_Position.x+= m_Speed*dt.asSeconds();
-    }
+    if (m_MovingLeft && m_Position.x>0)
+        m_Position.x -= m_Speed * dt.asSeconds();
+    if (m_MovingRight && (m_Position.x+m_Shape.getLocalBounds().width)<resolution.x)
+        m_Position.x += m_Speed * dt.asSeconds();
     m_Shape.setPosition(m_Position);
 }
